3e5144ee4a6525183ce1f6626999d61360ba1237
Shane Powell <kenjara@hotmail.com>
2021-05-15 16:37:42 +0100

    Add project files.

---
+++ MonitoringFunctions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.Versioning;
+using System.Threading;
+
+namespace SystemMonitorLib
+{
+    [SupportedOSPlatform("windows")]
+    public static class MonitoringFunctions
+    {
+        [SupportedOSPlatform("windows")]
+        public static int GetCpuUsage()
+        {
+            return GetPerformanceCounter("Processor", "% Processor Time", "_Total");
+        }
+
+        [SupportedOSPlatform("windows")]
+        public static int GetPerformanceCounter(string category, string counterName, string instanceName)
+        {
+            var performanceCounter = new PerformanceCounter(category, counterName, instanceName, Environment.MachineName);
+            _ = performanceCounter.NextValue();
+            Thread.Sleep(1000);
+            return (int)performanceCounter.NextValue();
+        }
+    }
+}
---
+++ SampleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SystemMonitorLib;
+
+namespace SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine($"CPU  {MonitoringFunctions.GetCpuUsage()}");
+            }
+            
+        }
+    }
+}
