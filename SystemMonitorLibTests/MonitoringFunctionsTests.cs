4ebadce59834ebc15c610ce680ad0e1fa5b98b57
Shane Powell <kenjara@hotmail.com>
2021-05-17 08:49:26 +0100

    Moved test project.

---
+++ SystemMonitorLibTests/MonitoringFunctionsTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SystemMonitorLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemMonitorLib.Tests
+{
+    [TestClass()]
+    public class MonitoringFunctionsTests
+    {
+        [TestMethod()]
+        public void GetCpuUsageTest()
+        {
+            var result = MonitoringFunctions.GetCpuUsage();
+
+            Assert.IsTrue(result is >= 0 and <= 100);
+        }
+    }
+}
